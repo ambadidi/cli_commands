f17639adef0838963dbfae01ec061624efd0f2ab
ambadidi <amine.badidi@hotmail.com>
2023-08-09 20:49:03 +0100

    wip

--- src/ls.rs
+++ src/ls.rs
@@ -1,19 +1,37 @@
-use std::ffi::OsString;
 use std::fs;
 use std::path::PathBuf;
 
 pub fn ls(args: &Vec<String>) {
     if args.len() == 2 {
-        let entries: Vec<OsString> = fs::read_dir(".")
+        let mut entries: Vec<PathBuf> = fs::read_dir(".")
             .unwrap()
-            .map(|res| res.map(|e| e.file_name()))
+            .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, _>>()
             .unwrap();
+        entries.sort_by(|a,b| {
+            let a_str = a.file_name().unwrap().to_string_lossy();
+            let b_str = b.file_name().unwrap().to_string_lossy();
+            if a_str.to_lowercase() == b_str {
+                std::cmp::Ordering::Greater
+            } else if b_str.to_lowercase() == a_str {
+                std::cmp::Ordering::Less
+            } else {
+                a_str.to_lowercase().cmp(&b_str.to_lowercase())
+            }
+        });
         for entry in entries {
-            if entry.to_str().map(|f| f.starts_with(".")).unwrap() {
+            let bold = "\x1b[1m";
+            let blue = "\x1b[34m";
+            let reset = "\x1b[0m";
+            let file_name = entry.file_name().unwrap().to_string_lossy();
+            if &file_name[0..1] == "." {
                 continue;
             }
-            print!("{}  ", entry.to_string_lossy());
+            if entry.is_dir() {
+                print!("{}{}{}{}  ",bold, blue ,file_name, reset);
+            } else {
+                print!("{}{}  ",file_name, reset);
+            }
         }
         println!();
         return;
@@ -41,7 +59,13 @@ pub fn ls(args: &Vec<String>) {
             if b_str.starts_with(".") && b_str.len() >= 2 {
                 b_str = &b_str[1..];
             }
-            a_str.cmp(b_str)
+            if a_str.to_lowercase() == b_str.to_string() {
+                std::cmp::Ordering::Greater
+            } else if b_str.to_lowercase() == a_str.to_string() {
+                std::cmp::Ordering::Less
+            } else {
+                a_str.to_lowercase().cmp(&b_str.to_lowercase())
+            }
         });
         for i in 0..entries.len() {
             for j in 0..entries.len() - i - 1 {
